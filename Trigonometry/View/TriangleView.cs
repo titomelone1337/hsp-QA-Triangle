21cb9d36920b5c0331a45e7c3bb3360082bf89c0
Tiago Melo <2017.itm.tiagomelo@gmail.com>
2025-04-10 22:07:39 +0100

    MViewC_Class

---
+++ Trigonometry/View/TriangleView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainTest;
+
+namespace Trigonometry.View
+{
+    public class TriangleView
+    {
+        public void ShowTriangle (Triangle t)
+        {
+            Console.WriteLine(t.ToString());
+            Console.WriteLine("Area: " + t.Area().ToString("F2"));
+        }
+        public void ShowError(string message)
+        {
+            Console.WriteLine("Error:" + message);
+        }
+    }
+}
