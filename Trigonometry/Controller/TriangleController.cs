c70e132a8f502a38cf9bc5d0e9d43be77aca21ea
Tiago Melo <2017.itm.tiagomelo@gmail.com>
2025-04-10 22:12:56 +0100

    MVController_class

---
+++ Trigonometry/Controller/TriangleController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using DomainTest;
+using Trigonometry.View;
+
+namespace Trigonometry.Controller
+{
+   public class TriangleController
+    {
+        private Triangle triangle;
+        private TriangleView view;
+        
+        public TriangleController()
+        {
+            triangle = new Triangle(1,2,3);
+            view = new TriangleView();
+        }
+
+        public void Run()
+        {
+            if (triangle.WasDefaulted)
+            {
+                view.ShowError("Invalid triangle values. Using default triangle");
+            }
+
+            view.ShowTriangle(triangle);
+        }
+       
+    }
+}
